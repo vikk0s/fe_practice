3c082b81a57fcd2d2d63223ef918933c59ddbfd3
vikko <vikkov@metropolia.fi>
2024-08-28 12:19:18 +0300

    Second commit

--- src/components/Random.js
+++ src/components/Random.js
@@ -4,7 +4,7 @@ const Random = (props) => {
     const randomValue = Math.floor(Math.random() * (props.max - props.min + 1)) + props.min
 
   return (
-    <div>Random value between {props.min} and {props.max} => {randomValue}</div>
+    <div>Random value between {props.min} and {props.max} ={'>'} {randomValue}</div>
   )
 }
 
